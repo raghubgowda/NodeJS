1c4febe8754ac7ae39e63c7923f5e0d94bcc54ae
Raghavendra M Boregowda <raghubgowda@Raghus-Mac.local>
2020-04-12 20:12:14 +0200

    Added asyncawait example

---
+++ callBacksPromiseAsyncAwait/asyncAwaitExample.js
@@ -0,0 +1,66 @@
+const log = console.log;
+let users = [];
+
+
+//Populates random users
+function populateUsers(callback){
+    let i = 1;
+    while( i <= 5){
+        const user = { 
+            id : i,
+            name : `User${i}`,
+            age : Math.round(Math.random() * 100)
+        };
+        users.push(user); 
+        i++;
+    }
+}
+
+//Returns the user matching the user id
+function getUser(userId, callback){
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve(users.find(user => user.id === userId));
+        }, 2000);
+    });
+    
+}
+
+//Print User Details of the user passed
+function getUserDetails(user, callback){
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            if(user == null){
+                reject(new Error(`User with id ${userId} not found!!!`));
+            }
+            else{
+                resolve(`User - Id: ${user.id} Name: ${user.name} Age: ${user.age}`);  
+            }
+        }, 3000); 
+    });
+}
+
+
+//Populate the users
+populateUsers();
+
+//Generate a random user id
+const userId = Math.round(Math.random() * 10);
+
+log(`Fetching details for the userId: ${userId}`);
+
+//Fetch the user and print the details
+async function displayUserDetails(){
+    try {
+        const user = await getUser(userId);    
+        const userDetails = await getUserDetails(user);
+        log(userDetails);
+    } catch (error) {
+        log(error.message);
+    }
+}
+
+displayUserDetails();
+
+
+
